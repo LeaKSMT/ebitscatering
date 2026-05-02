fdfbc14a43fc11874098bd73fdbabb336872b8b0
KerbyCaspe <caspekerby969@gmail.com>
2026-05-02 10:36:43 +0800

    Fix home navbar links

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -423,6 +423,7 @@ function Home() {
 
                     <Link
                         to="/login"
+                        className="inline-flex items-center justify-center rounded-2xl border border-white/15 bg-white/10 px-4 py-2.5 font-semibold text-white transition hover:-translate-y-0.5 hover:bg-white hover:text-[#0b4d3b]"
                     >
                         Login
                     </Link>
